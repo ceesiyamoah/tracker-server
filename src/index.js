9af1bcdf9930978580c361612fa25343bbcd462c
ceesiyamoah <ccyamoah@gmail.com>
2020-12-12 02:54:45 +0000

    track routes completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 require('./models/User');
+require('./models/Tracks');
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const bodyParser = require('body-parser');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const MONGO_URI =
 	'mongodb+srv://admin:passwordPassword@cluster0.aoc76.mongodb.net/<dbname>?retryWrites=true&w=majority';
